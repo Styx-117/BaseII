750b7bd00c2900518e6ee5c8bf7ac01871f27e86
Styx117 <Styx117@outlook.com.pe>
2026-07-11 02:45:38 -0500

    Pre-release 1

--- PROYECTOS/SISTEMA-FRONTED/js/auth.js
+++ PROYECTOS/SISTEMA-FRONTED/js/auth.js
@@ -1,5 +1,5 @@
 export const login = async (email, password) => {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
+    const response = await fetch('https://baseii.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
