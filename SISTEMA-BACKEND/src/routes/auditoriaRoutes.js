aff45e06d2637d9d3571bc85c5932cadc4c286af
Moon26sc <92294396+Moon26sc@users.noreply.github.com>
2026-07-11 05:58:08 -0500

    Delete SISTEMA-BACKEND directory

--- SISTEMA-BACKEND/src/routes/auditoriaRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { obtenerAuditoriaPrecios } = require('../controllers/auditoriaController');
-
-router.get('/', obtenerAuditoriaPrecios);
-module.exports = router;
--- SISTEMA-BACKEND/src/routes/categoriaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { obtenerCategorias, crearCategoria } = require('../controllers/CategoriaController');
-
-router.get('/', obtenerCategorias);
-router.post('/', crearCategoria);
-
-module.exports = router;
