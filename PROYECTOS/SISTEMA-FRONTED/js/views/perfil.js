8f9de037cb9f50bdd979177f6293a19f7ff107fc
Moon26sc <mayrasaravia2605004@gmail.com>
2026-07-13 00:03:13 -0500

    "editar perfil prueba 2"

--- PROYECTOS/SISTEMA-FRONTED/js/views/perfil.js
+++ PROYECTOS/SISTEMA-FRONTED/js/views/perfil.js
@@ -24,7 +24,7 @@ export async function renderPerfil(container) {
                                 <input type="text" id="perfil_nombre" class="form-control" value="${user.nombre_completo}" required>
                             </div>
                             <div class="mb-3">
-                                <label class="form-label text-muted small fw-bold">Correo Electrónico (Login)</label>
+                                <label class="form-label text-muted small fw-bold">Correo Electrónico</label>
                                 <input type="email" id="perfil_email" class="form-control" value="${user.email}" required>
                             </div>
                             
